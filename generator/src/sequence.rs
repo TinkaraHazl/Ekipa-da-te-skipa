ef1441a708e677ea5d3ab3f1bb177f2b014b0ed5
Jaka Sprah <jaka.sprah@gmail.com>
2024-09-02 10:32:47 +0200

    obvezna zaporedja (razen drop)

--- generator/src/sequence.rs
+++ generator/src/sequence.rs
@@ -2,10 +2,13 @@ enum Sequence {
     Geometric,
     Constant,
     Arithmetic,
+    Product,
     Sum
 }
 
 pub mod geometric;
 pub mod constant;
 pub mod arithmetic;
+pub mod product;
 pub mod sum;
+pub mod models;
--- generator/src/sequence/product.rs
+++ generator/src/sequence/product.rs
@@ -1,23 +1,23 @@
 use crate::Range;
 use super::models::Sequence;
 
-pub struct Sum<S1, S2> {
+pub struct Produkt<S1, S2> {
     zaporedje1: S1,  
     zaporedje2: S2, }
 
 
 
-impl<S1, S2> Sum<S1, S2> 
+impl<S1, S2> Produkt<S1, S2> 
 where
     S1: Sequence<f64>, 
     S2: Sequence<f64>,  
 {
-    pub fn nov(zaporedje1: S1, zaporedje2: S2) -> Sum<S1, S2> {
-        Sum { zaporedje1, zaporedje2 }
+    pub fn nov(zaporedje1: S1, zaporedje2: S2) -> Produkt<S1, S2> {
+        Produkt { zaporedje1, zaporedje2 }
     }
 
     pub fn k_th(&self, k: usize) -> f64 {
-        self.zaporedje1.k_th(k) + self.zaporedje2.k_th(k)
+        self.zaporedje1.k_th(k) * self.zaporedje2.k_th(k)
     }
 
     pub fn range(&self, range: Range) -> Vec<f64> {
@@ -30,5 +30,3 @@ where
         result
     }
 }
-
-
