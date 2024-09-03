f9a81514b91fabc966b99d5a3a15741874d2c4b9
Tinkara Hazl <th52887@student.uni-lj.si>
2024-09-03 17:51:57 +0200

    baza popravljena

--- generator/src/sequence/base.rs
+++ generator/src/sequence/base.rs
@@ -22,38 +22,41 @@ fn b_to_ten (t: f64, b: usize) -> f64 {
     let mut s: f64 = 0.0;
     let int: f64 = t.trunc();
     let frac: f64 = t - int;
-    let mut int_str: String = int.to_string();
-    let frac_str: String = frac.to_string();
-    let lw: usize = int_str.chars.count();
-    let lf: usize  = frac_str.chars.count();
 
+    let mut int_str: String = int.to_string();
+    let lw: usize = int_str.len();
     for i in 0..lw {
-        s += int_str.pop() * b.pow(i)
+        s += int_str.pop().unwrap().to_digit(10).unwrap() as f64 * (b.pow(i as u32) as f64)
     }
+
+    let frac_str: String = frac.to_string();
+    let lf: usize  = frac_str.len();
     for i in 3..lf {
-        s += frac_str.chars().nth(i).unwrap() * b.pow(-(i - 2))
+        s += frac_str.chars().nth(i).unwrap().to_digit(10).unwrap() as f64 / (b.pow((i - 2) as u32) as f64)
     }
+    s
 }
 
 fn ten_to_b (t: f64, n: usize) -> f64 {
-    let s: f64 = 0.0;
+    let mut s: f64 = 0.0;
     let mut int_t = t.trunc();
     let mut frac_t = t - int_t;
-    while int_t > 0 {
-        s += int_t % n;
-        int_t /= n
+    while int_t > 0.0 {
+        s += int_t % (n as f64);
+        int_t /= n as f64
     }
     for i in 1..11 {
-        s += ((frac_t * n) / 1).powi(-i);
-        frac_t = (frac_t * n) % 1
+        s += ((frac_t * (n as f64)) / 1.0).powi(-i);
+        frac_t = (frac_t * (n as f64)) % 1.0
     }
+    s
 }
 
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn ten_to_b() {
-        assert_eq!(ten_to_b(3.0, 2.0), 11)
-    }
-}
+//#[cfg(test)]
+//mod tests {
+//    #[test]
+//    fn ten_to_b() {
+//        assert_eq!(ten_to_b(3.0, 2), 11.0)
+//    }
+//}
